6c884ef5a6382fcda435656adb3afa9550d3b843
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-22 19:48:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
-# require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 end
+
+# :projects => {
+  # title => {
+    # image -> image_link
+    # description
+    # location
+    # funding -> percent_funded
+  # }
+# }
