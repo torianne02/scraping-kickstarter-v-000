6ce2da42d3317cc11f4ab5934b7a3e30a030d816
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-22 20:12:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,10 +4,22 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
 end
 
 create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image_link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
 
 # :projects => {
   # title => {
